315d19bd0644360edb8754b3336bc306652715f2
Urek Michiko <urekmich@at-h3.msk.21-school.ru>
2022-02-12 22:47:15 +0300

    1234

--- src/game_of_life1.c
+++ src/game_of_life1.c
@@ -5,7 +5,9 @@
 
 void init(int** mas, int param);
 void clear_mas(int** mas);
-void scan_mas(int** mas);
+int scan_mas(int** mas);
+void generate_field(int** mas, int param);
+
 int main() {
     int **mas = NULL;
 
@@ -14,29 +16,47 @@ int main() {
     return 0;
 }
 
-void init (int** mas, int param) {
-    clear_mas(int** mas);
-    scan_mas(int** mas);
+int init (int** mas, int param) {
+    clear_mas(mas);
+    if (param == 0)
+        return scan_mas(mas);
+    else
+        generate_field(mas);
+    return 0;
 }
 
 void clear_mas(int** mas) {
-    for(int j = 0; j < FILED_H; j++)
-        for(int i = 0; i < FILED_W; i++)
+    for(int i = 0; i < FILED_H; i++)
+        for(int j = 0; j < FILED_W; j++)
             mas[i][j] = 0;
 }
 
-void scan_mas(int** mas) {
-    int i = 0, int j = 0;
-    while
-    char ch = getchar();
-        if (ch = '1')
-        { mas[i][j] = 1;
-        } else {
-             if( ch = '0'){
+int scan_mas(int** mas) {
+    int i = 0, j = 0; 
+    while(1) {
+        int ch = getchar();
+        if (ch == '1') {
+            if (j < FILED_W && i < FILED_H) {
+                mas[i][j] = 1;
+                j++;
             } else {
-                if( ch = 'EOF')
-                { break;
-                    } else { i++; j = 0}
-                    }
-                }
+                return 1;
+            }
+        } else if ( ch == '0') {
+            if (j < FILED_W && i < FILED_H) {
+                mas[i][j] = 0;
+                j++;
+            } else {
+                return 1;
+            }
+        } else if (ch == EOF) {
+            return 0;
+        } else if (ch == '\n') {
+            i++; 
+            j = 0;
+        } else {
+            return 1;
+        }
+    }
+    return 0;
 }
