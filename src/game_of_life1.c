9c234fc32193a8b851e55f4aac7ae49fa870b678
Urek Michiko <urekmich@ga-l3.msk.21-school.ru>
2022-02-12 18:25:45 +0300

    Rus

---
+++ src/game_of_life1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define FILED_W 80
+#define FILED_H 25
+
+void init(int** mas, int param);
+void clear_mas(int** mas);
+void scan_mas(int** mas);
+int main() {
+    int **mas = NULL;
+
+    init(int** mas, param)
+
+    return 0;
+}
+
+void init (int** mas, int param) {
+    clear_mas(int** mas);
+    scan_mas(int** mas);
+}
+
+void clear_mas(int** mas) {
+    for(int j = 0; j < FILED_H; j++)
+        for(int i = 0; i < FILED_W; i++)
+            mas[i][j] = 0;
+}
+
+void scan_mas(int** mas) {
+    int i = 0, int j = 0;
+    while
+    char ch = getchar();
+        if (ch = '1')
+        { mas[i][j] = 1;
+        } else {
+             if( ch = '0'){
+            } else {
+                if( ch = 'EOF')
+                { break;
+                    } else { i++; j = 0}
+                    }
+                }
+}
